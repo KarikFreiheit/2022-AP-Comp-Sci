59d94b6c545c30124579be753a1783bc4de4ee0b
KarikFreiheit <crossoverkarik@gmail.com>
2022-09-14 10:46:23 -0700

    12 Days Start

---
+++ GS1/Twelve Days/src/main.java
@@ -0,0 +1,28 @@
+public class main {
+
+    public static void main(){
+
+        first();
+        love();
+        partridge();
+        second();
+
+    }
+
+    public static void first(){
+        System.out.println("On the first day of Christmas,");
+
+    }
+    public static void love(){
+        System.out.println(" my true love sent to me");
+    }
+
+    public static void partridge(){
+        System.out.println("A partridge in a pear tree");
+    }
+
+    public static void second(){
+        System.out.println("On the first day of Christmas,");
+
+    }
+}
