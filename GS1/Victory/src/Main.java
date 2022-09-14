7ebf66d7e19747646940badd777bccfcc8999988
KarikFreiheit <crossoverkarik@gmail.com>
2022-09-14 09:36:46 -0700

    Victory(Finished)

---
+++ GS1/Victory/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("//////////////////////");
+        victory();
+
+    }
+    public static void victory(){
+        for( int i = 0; i <= 4; i++) {
+            System.out.println("|| Victory is mine! ||");
+            System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        }
+
+    }
+
+
+}
