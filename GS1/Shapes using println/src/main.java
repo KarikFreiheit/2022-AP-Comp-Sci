3212bc1d2ae7f2e490d6b7b8f66abcc8154e45ba
KarikFreiheit <crossoverkarik@gmail.com>
2022-09-14 09:55:36 -0700

    Shapes using println

--- GS1/Shapes using println/src/main.java
+++ GS1/Shapes using println/src/main.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class main {
+public class main {
+
+        public static void main(String[] args) {
+            top();
+            bottom();
+            middle();
+            top();
+            bottom();
+            middle();
+            bottom();
+            top();
+            middle();
+            bottom();
+        }
+        public static void top(){
+            System.out.println("  -------  \n /       \\ \n/         \\");
+
+        }
+
+        public static void middle(){
+            System.out.println("-\"-'-\"-'-\"-");
+        }
+        public static void bottom(){
+            System.out.println("\\         /\n \\       / \n  -------  ");
+
+        }
 }
