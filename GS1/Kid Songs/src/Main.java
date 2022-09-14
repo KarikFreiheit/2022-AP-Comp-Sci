9d1d8b53bcab38be6e0331efe3aa094e628225a6
KarikFreiheit <crossoverkarik@gmail.com>
2022-09-14 10:09:02 -0700

    Kid Songs Test

---
+++ GS1/Kid Songs/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("There was an old lady who swallowed a fly");
+        fly();
+        spider();
+        System.out.println("She swallowed the spider to catch the fly;");
+    }
+
+    public static void fly(){
+        System.out.println("I don’t know why she swallowed a fly Perhaps she’ll die!");
+    }
+
+    public static void spider(){
+        System.out.println("There was an old lady who swallowed a spider \n That wriggled and jiggled and tickled inside her!");
+    }
+
+    public static void bird(){
+
+    }
+}
