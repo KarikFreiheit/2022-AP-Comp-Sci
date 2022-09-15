3a86ddee8d64ca61f8c9005045b2a41264012e6b
KarikFreiheit <crossoverkarik@gmail.com>
2022-09-15 09:57:51 -0700

    12 Days Finished

--- GS1/Twelve Days/src/main.java
+++ GS1/Twelve Days/src/main.java
@@ -1,35 +1,231 @@
 public class main {
 
-    public static void main(){
+    public static void main(String[] args){
 
         first();
         love();
         partridge();
+
         second();
         love();
         doves();
         partridge();
 
+        third();
+        love();
+        hens();
+        doves();
+        partridge();
+
+        fourth();
+        love();
+        birds();
+        hens();
+        doves();
+        partridge();
+
+        fifth();
+        love();
+        rings();
+        birds();
+        hens();
+        doves();
+        partridge();
+
+        sixth();
+        love();
+        geese();
+        rings();
+        birds();
+        hens();
+        doves();
+        partridge();
+
+        seventh();
+        love();
+        swans();
+        geese();
+        rings();
+        birds();
+        hens();
+        doves();
+        partridge();
+
+        eighth();
+        love();
+        maids();
+        swans();
+        geese();
+        rings();
+        birds();
+        hens();
+        doves();
+        partridge();
+
+        ninth();
+        love();
+        ladies();
+        maids();
+        swans();
+        geese();
+        rings();
+        birds();
+        hens();
+        doves();
+        partridge();
+
+        tenth();
+        love();
+        lords();
+        ladies();
+        maids();
+        swans();
+        geese();
+        rings();
+        birds();
+        hens();
+        doves();
+        partridge();
+
+        eleventh();
+        love();
+        pipers();
+        lords();
+        ladies();
+        maids();
+        swans();
+        geese();
+        rings();
+        birds();
+        hens();
+        doves();
+        partridge();
+
+        twelfth();
+        love();
+        drummers();
+        pipers();
+        lords();
+        ladies();
+        maids();
+        swans();
+        geese();
+        rings();
+        birds();
+        hens();
+        doves();
+        partridge();
     }
 
     public static void first(){
         System.out.println("On the first day of Christmas,");
 
     }
-    public static void love(){
-        System.out.println(" my true love sent to me");
-    }
-
-    public static void partridge(){
-        System.out.println("A partridge in a pear tree");
-    }
-
     public static void second(){
         System.out.println("On the second day of Christmas,");
 
+    }
+    public static void third(){
+        System.out.println("On the third day of Christmas,");
+
+    }
+
+    public static void fourth(){
+        System.out.println("On the fourth day of Christmas,");
+
+    }
+
+    public static void fifth(){
+        System.out.println("On the fifth day of Christmas,");
+
+    }
+
+    public static void sixth(){
+        System.out.println("On the sixth day of Christmas,");
+
+    }
+
+    public static void seventh(){
+        System.out.println("On the seventh day of Christmas,");
+
+    }
+
+    public static void eighth(){
+        System.out.println("On the eighth day of Christmas,");
+
+    }
+
+    public static void ninth(){
+        System.out.println("On the ninth day of Christmas,");
+
+    }
+
+    public static void tenth(){
+        System.out.println("On the tenth day of Christmas,");
+
+    }
+
+    public static void eleventh(){
+        System.out.println("On the eleventh day of Christmas,");
+
+    }
+
+    public static void twelfth(){
+        System.out.println("On the twelfth day of Christmas,");
+
+    }
+    public static void love(){
+
+        System.out.println("my true love sent to me");
+    }
+
+    public static void partridge(){
+
+        System.out.println("A partridge in a pear tree\n");
+    }
+
+    public static void hens(){
+        System.out.println("three French hens,");
+    }
+
+    public static void birds(){
+        System.out.println("four calling birds,");
+    }
+
+    public static void rings(){
+        System.out.println("five golden rings");
+    }
+
+    public static void geese(){
+        System.out.println("six geese a-laying,");
+    }
+
+    public static void swans(){
+        System.out.println("seven swans a-swimming,");
+    }
+
+    public static void maids(){
+        System.out.println("eight maids a-milking,");
+    }
+
+    public static void ladies(){
+        System.out.println("nine ladies dancing,");
+    }
+
+    public static void lords(){
+        System.out.println("ten lords a-leaping,");
+    }
+
+    public static void pipers(){
+        System.out.println("eleven pipers piping,");
+    }
+
+    public static void drummers(){
+        System.out.println("Twelve drummers drumming,");
     }
 
     public static void doves(){
         System.out.println("Two turtle doves, and");
     }
+
 }
