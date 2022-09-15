8f1402d0a2efbfad41328f3efd4e628ff8065cba
KarikFreiheit <crossoverkarik@gmail.com>
2022-09-15 10:33:05 -0700

    Squares Finished

---
+++ GS2/Output Squares/src/main.java
@@ -0,0 +1,16 @@
+public class main {
+
+    public static void main(String[] args){
+
+        squares();
+
+    }
+    public static void squares(){
+        for(int i = 1; i <= 10; i++ ){
+
+            System.out.println(i * i);
+
+        }
+    }
+
+}
