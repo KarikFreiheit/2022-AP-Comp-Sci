a61fa66b16ff9258a06de8fef3a05eeea9c7cd35
KarikFreiheit <crossoverkarik@gmail.com>
2022-12-02 09:36:53 -0800

    Finished ReverseWordsandLines: Reverses the line order and the word order of the lines.

--- GS7/ReverseWordsandLines/src/Main.java
+++ GS7/ReverseWordsandLines/src/Main.java
@@ -7,19 +7,35 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main{
 
     public static void main(String[] args) throws FileNotFoundException {
-
-        Scanner fileSC = new Scanner(new File("C:\\Users\\steel\\Desktop\\AP Comp Sci\\GS7\\ReverseWordsandLines\\src\\this.txt"));
+        File file = new File("this.txt");
+        Scanner fileSC = new Scanner(file);
         ArrayList<String> lines = new ArrayList<>();
+        ArrayList<String> words = new ArrayList<>();
+        ArrayList<String> newline = new ArrayList<>();
         while(fileSC.hasNextLine()){
             lines.add(fileSC.nextLine());
         }
+        System.out.println("Orignal: " + lines);
 
-        System.out.println(lines);
+        for (String s : lines) {
+            Scanner sc = new Scanner(s);
+            while(sc.hasNext()){
+                words.add(sc.next());
+            }
+            Collections.reverse(words);
+            newline.add(words.toString().replace(",", ""));
+            words.clear();
+        }
+
+        Collections.reverse(newline);
+        System.out.println("Reversed: " + newline);
 
 
     }
