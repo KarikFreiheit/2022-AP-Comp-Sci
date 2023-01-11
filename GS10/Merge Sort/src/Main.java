629d0b2334e9a206d6b8e61b00244b3cd2610e2f
KarikFreiheit <crossoverkarik@gmail.com>
2023-01-11 09:58:31 -0800

    Finished merge sort of words.

--- GS10/Merge Sort/src/Main.java
+++ GS10/Merge Sort/src/Main.java
@@ -1,31 +1,54 @@
-import java.util.Arrays;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args){
-        //REMEMBER TO GET INPUT FROM FILE BEFORE SUBMITTING and use strings
-        String[] words = new String[]{"Hello, Yes, No, Bus, Apple, Mouse, World"};
-        int[] numbers = new int[]{4, 7, 2, 9, 1, 8, 5, 6, 10, 3};
-        System.out.println(Arrays.toString(mergeSort(numbers)));
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("words.txt"));
+        String[] words = new String[9995]; // Number of words in words.txt
+        int i = 0;
+        while(sc.hasNext()) {
+            words[i] = sc.nextLine();
+            i++;
+        }
+        System.out.println("Unsorted: " + Arrays.toString(words));
+
+        mergeSort(words);
+        System.out.println("Sorted: " + Arrays.toString(words));
 
     }
 
 
-    public static int[] mergeSort(int[] array){
+    public static void mergeSort(String[] array){
         if (array.length > 1) {
-            int[] left = Arrays.copyOfRange(array, 0, array.length / 2);
-            int[] right = Arrays.copyOfRange(array, array.length / 2, array.length);
+            String[] left = Arrays.copyOfRange(array, 0, array.length / 2);
+            String[] right = Arrays.copyOfRange(array, array.length / 2, array.length);
 
 
             mergeSort(left);
             mergeSort(right);
+
+            merge(array, left, right);
+
         }
 
-        int l = 0;
-        int r = 0;
-
-
-        return array;
     }
+
+    public static void merge(String[] result, String[] left, String[] right){
+        int i1 = 0;
+        int i2 = 0;
+        for(int i = 0; i < result.length; i++){
+            if(i2 >= right.length || (i1 < left.length && left[i1].charAt(0) <= right[i2].charAt(0)) ){
+                result[i] = left[i1];
+                i1++;
+            }else{
+                result[i] = right[i2];
+                i2++;
+            }
+        }
+
+    }
+
 
 }
