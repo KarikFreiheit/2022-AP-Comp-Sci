2fb67814d051ccd76bad9c41a3b91bd564f96e64
KarikFreiheit <crossoverkarik@gmail.com>
2023-01-19 10:14:17 -0800

    Finished sumTo

--- GS11/writeSequence/src/Main.java
+++ GS11/writeSequence/src/Main.java
@@ -1,23 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        writeSequence(5);
+        writeSequence(8);
     }
 
     public static void writeSequence(int n){
         if(n < 1){
             throw new IllegalArgumentException();
         }
-        if (n >= 1 && n % 2 == 0) {
+        if (n >= 0) {
             System.out.print(n + " ");
             writeSequence(n-1);
             if(n != 1) {
                 System.out.print(n + " ");
-            }
-        }else if (n >= 3){
-            System.out.print(n - 2 + " ");
-            writeSequence(n-1);
-            if(n != 1) {
-                System.out.print(n - 2 + " ");
             }
         }
     }
