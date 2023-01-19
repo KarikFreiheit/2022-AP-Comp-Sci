286f797a13491c74873ec1a66becd81147358840
KarikFreiheit <crossoverkarik@gmail.com>
2023-01-19 09:34:56 -0800

    Interesting Pattern I made but still haven't gotten anywhere.

--- GS11/writeSequence/src/Main.java
+++ GS11/writeSequence/src/Main.java
@@ -1,16 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        writeSequence(4);
+        writeSequence(5);
     }
 
     public static void writeSequence(int n){
         if(n < 1){
             throw new IllegalArgumentException();
         }
-        if (n >= 3) {
+        if (n >= 1 && n % 2 == 0) {
+            System.out.print(n + " ");
+            writeSequence(n-1);
+            if(n != 1) {
+                System.out.print(n + " ");
+            }
+        }else if (n >= 3){
             System.out.print(n - 2 + " ");
             writeSequence(n-1);
-            if(n - 2 != 1) {
+            if(n != 1) {
                 System.out.print(n - 2 + " ");
             }
         }
