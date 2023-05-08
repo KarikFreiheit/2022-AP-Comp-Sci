3e0e8bbb09fe687b72a8f8f77879f3766abdbe39
KarikFreiheit <crossoverkarik@gmail.com>
2023-05-08 11:51:13 -0700

    2 Questions Left - Page 621

--- GS13/Self-Check-Problems/src/Main.java
+++ GS13/Self-Check-Problems/src/Main.java
@@ -1,5 +1,74 @@
 public class Main {
     public static void main(String[] args) {
+    //8
+        //Answer: B 2AB 1/nD 2CD 1\nA 2AA  1/nC 2CC 1
+    //9
+        /*Answer:
+        flute
+        shoe 1
+        flute 2
+        flute
+        blue 1
+        flute 2
+        moo
+        moo 1
+        moo 2
+        moo
+        blue 1
+        moo 2
 
+        */
+    //10
+    /*Answer:
+        moo 2
+        blue 1
+        moo
+        moo 1
+        moo 2
+        moo
+        shoe 1
+        flute 2
+        flute
+        blue 1
+        flute 2
+        flute
+     */
+    //11 Answer:
+    /*
+        squid
+        creature 1
+        squid 2
+        BIG!
+        spout
+        creature 2
+        ocean-dwelling
+        creature 1
+        creature 2
+        ocean-dwelling
+        warm-blooded
+        creature 2
+     */
+    //12 Answer:
+        /*
+        creature 2
+        ocean-dwelling
+        creature 1
+        tentacles
+        squid
+        creature 1
+        creature 2
+        ocean-dwelling
+        warm-blooded
+        creature 2
+        BIG!
+        spout
+         */
+    //13 Answer:
+        /*
+
+         */
+    //14 Answer:
+    //16 Answer: No, because you can just use everything in the rectangle class with a square so unless you want to add
+        // more stuff to the class there would be no point to it.
     }
 }
